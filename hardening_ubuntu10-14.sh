0111873f6f180844d1b2c86685a090794927f69b
Zalas heribertus <99477783+zalas07@users.noreply.github.com>
2025-06-02 16:51:04 +0700

    Update hardening_ubuntu10-14.sh
    melakukan perubahan pada script hardening di bagian auditd

--- hardening_ubuntu10-14.sh
+++ hardening_ubuntu10-14.sh
@@ -542,9 +542,9 @@ EOF
     sudo sed -i 's/^max_log_file *=.*/max_log_file = 200/' /etc/audit/auditd.conf
     sudo sed -i 's/^max_log_file_action *=.*/max_log_file_action = keep_logs/' /etc/audit/auditd.conf
     sudo sed -i 's/^space_left *=.*/space_left = 90/' /etc/audit/auditd.conf
-    sudo sed -i 's/^space_left_action *=.*/space_left_action = ROTATE/' /etc/audit/auditd.conf
+    sudo sed -i 's/^space_left_action *=.*/space_left_action = SYSLOG/' /etc/audit/auditd.conf
     sudo sed -i 's/^admin_space_left *=.*/admin_space_left = 80/' /etc/audit/auditd.conf
-    sudo sed -i 's/^admin_space_left_action *=.*/admin_space_left_action = ROTATE/' /etc/audit/auditd.conf
+    sudo sed -i 's/^admin_space_left_action *=.*/admin_space_left_action = SUSPEND/' /etc/audit/auditd.conf
     echo "[✓] auditd.conf dikonfigurasi."
 
     # === Reload Rules ===
